ce48195eea3cc6cd85a17698ecfbfcf54645847d
Petar Lalov <petar.lalov@gmail.com>
2022-08-14 22:29:41 +0300

    Firmaware is Working with external CAN device. RX filter receives everithing.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,11 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "print.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -39,19 +43,22 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+CAN_HandleTypeDef hcan;
 
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 uint8_t ubKeyNumber = 0x0;
-CAN_HandleTypeDef CanHandle;
 CAN_TxHeaderTypeDef TxHeader;
 CAN_RxHeaderTypeDef RxHeader;
 uint8_t TxData[8];
 uint8_t RxData[8];
-uint32_t TxMailbox;
+uint32_t TxMailbox=0;
 
 CAN_FilterTypeDef sFilterConfig;
+
+#define PRINT_BUFFER_SIZE 256
+char printBuffer[PRINT_BUFFER_SIZE];
 
 /* USER CODE END PV */
 
@@ -67,136 +74,226 @@ static void MX_CAN_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+void __print_service(const char *str) {
+    HAL_UART_Transmit(&huart2, (uint8_t*) str, strlen(str), 100);
+}
+
+//void _printf(const char *format, va_list args) {
+////	va_list args;
+////	va_start(args, format);
+//
+//	if(strlen(format) >= PRINT_BUFFER_SIZE) {
+//		const char* errorMessage = "ERROR: format argument has lenght biger than expected len!\n";
+//		_print(errorMessage);
+//		_print(format);
+//		_print("\r\n");
+//		return;
+//	}
+//
+//	int len = vsnprintf(printBuffer, PRINT_BUFFER_SIZE, format, args);
+////	va_end(args);
+//
+//	if(len > 0) {
+//		_print(printBuffer);
+//	}else{
+//		const char* errorMessage = "ERROR: To large string to format";
+//		_print(errorMessage);
+//		_print(format);
+//		_print("\r\n");
+//	}
+//}
+//
+//void println(const char *format, ...) {
+//	va_list args;
+//	va_start(args, format);
+//	_printf(format, args);
+//	va_end(args);
+//	_print("\r\n");
+//}
+//
+//void print(const char *format, ...) {
+//	va_list args;
+//	va_start(args, format);
+//	_printf(format, args);
+//	va_end(args);
+//}
+//
+////void print(const char *format, ...) {
+////	va_list args;
+////	va_start(args, format);
+////
+////	if(strlen(format) >= PRINT_BUFFER_SIZE) {
+////		const char* errorMessage = "ERROR: format argument has lenght biger than expected len!\n";
+////		_print(errorMessage);
+////		_print(format);
+////		_print("\r\n");
+////		return;
+////	}
+////
+////	int len = vsnprintf(printBuffer, PRINT_BUFFER_SIZE, format, args);
+////	va_end(args);
+////
+////	if(len > 0) {
+////		_print(printBuffer);
+////	}else{
+////		const char* errorMessage = "ERROR: To large string to format";
+////		_print(errorMessage);
+////		_print(format);
+////		_print("\r\n");
+////	}
+////}
+
+
+
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
-int main(void) {
-    /* USER CODE BEGIN 1 */
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
 
-    /* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-    /* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-    HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-    /* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
+  print_init(__print_service);
+  /* USER CODE END Init */
 
-    /* USER CODE END Init */
+  /* Configure the system clock */
+  SystemClock_Config();
 
-    /* Configure the system clock */
-    SystemClock_Config();
+  /* USER CODE BEGIN SysInit */
 
-    /* USER CODE BEGIN SysInit */
+  /* USER CODE END SysInit */
 
-    /* USER CODE END SysInit */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_CAN_Init();
+  /* USER CODE BEGIN 2 */
 
-    /* Initialize all configured peripherals */
-    MX_GPIO_Init();
-    MX_USART2_UART_Init();
-    MX_CAN_Init();
+  /* USER CODE END 2 */
 
-    /* USER CODE BEGIN 2 */
-
-    /* USER CODE END 2 */
-
-    /* Infinite loop */
-    /* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
     while (1) {
-        /* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-        /* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
+    	uint32_t error = HAL_CAN_GetError(&hcan);
+    	if(error != HAL_CAN_ERROR_NONE) {
+    		char binary_string[34];
+
+    		itoa(error, binary_string, 2);
+    		println("CAN Error is: %s", binary_string);
+    	}
+
         uint32_t RxFifo = 0;
-        uint32_t fillLevel = HAL_CAN_GetRxFifoFillLevel(&CanHandle, RxFifo);
+        uint32_t fillLevel = HAL_CAN_GetRxFifoFillLevel(&hcan, RxFifo);
 
         if (fillLevel == 0) {
             /* Set the data to be transmitted */
-            int pendingRequests = HAL_CAN_IsTxMessagePending(&CanHandle, TxMailbox);
+            int pendingRequests = HAL_CAN_IsTxMessagePending(&hcan, TxMailbox);
             if (pendingRequests == 0) {
                 TxData[0] = ubKeyNumber;
                 TxData[1] = 0xAD;
-                if (HAL_CAN_AddTxMessage(&CanHandle, &TxHeader, TxData, &TxMailbox) != HAL_OK) {
+                if (HAL_CAN_AddTxMessage(&hcan, &TxHeader, TxData, &TxMailbox) != HAL_OK) {
                     /* Transmission request Error */
                     Error_Handler();
                 }
             }
-//          HAL_Delay(10);
         } else {
-            HAL_CAN_GetRxMessage(&CanHandle, RxFifo, &RxHeader, RxData);
+            HAL_CAN_GetRxMessage(&hcan, RxFifo, &RxHeader, RxData);
+            if(RxHeader.IDE == CAN_ID_STD){
+                println("RX: StdId=%x; DLC=%d", RxHeader.StdId, RxHeader.DLC);
+            }else{
+                println("RX: ExtId=%x; DLC=%d", RxHeader.ExtId, RxHeader.DLC);
+            }
+            println("RX: %x%x%x%x%x%x%x%x", RxData[0], RxData[1], RxData[2], RxData[3], RxData[4], RxData[5], RxData[6], RxData[7]);
         }
+        HAL_Delay(500);
 
     }
-    /* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
-void SystemClock_Config(void) {
-    RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    /** Initializes the RCC Oscillators according to the specified parameters
-     * in the RCC_OscInitTypeDef structure.
-     */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-        Error_Handler();
-    }
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-    /** Initializes the CPU, AHB and APB buses clocks
-     */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
-        Error_Handler();
-    }
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /**
- * @brief CAN Initialization Function
- * @param None
- * @retval None
- */
-static void MX_CAN_Init(void) {
+  * @brief CAN Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_CAN_Init(void)
+{
 
-    /* USER CODE BEGIN CAN_Init 0 */
+  /* USER CODE BEGIN CAN_Init 0 */
 
-    /* USER CODE END CAN_Init 0 */
+  /* USER CODE END CAN_Init 0 */
 
-    /* USER CODE BEGIN CAN_Init 1 */
+  /* USER CODE BEGIN CAN_Init 1 */
 
-    /* USER CODE END CAN_Init 1 */
-    CanHandle.Instance = CAN1;
-    CanHandle.Init.Prescaler = 7;
-    CanHandle.Init.Mode = CAN_MODE_SILENT_LOOPBACK;
-    CanHandle.Init.SyncJumpWidth = CAN_SJW_1TQ;
-    CanHandle.Init.TimeSeg1 = CAN_BS1_4TQ;
-    CanHandle.Init.TimeSeg2 = CAN_BS2_4TQ;
-    CanHandle.Init.TimeTriggeredMode = DISABLE;
-    CanHandle.Init.AutoBusOff = DISABLE;
-    CanHandle.Init.AutoWakeUp = DISABLE;
-    CanHandle.Init.AutoRetransmission = DISABLE;
-    CanHandle.Init.ReceiveFifoLocked = DISABLE;
-    CanHandle.Init.TransmitFifoPriority = DISABLE;
-    if (HAL_CAN_Init(&CanHandle) != HAL_OK) {
-        Error_Handler();
-    }
-
-    /* USER CODE BEGIN CAN_Init 2 */
+  /* USER CODE END CAN_Init 1 */
+  hcan.Instance = CAN1;
+  hcan.Init.Prescaler = 32;
+  hcan.Init.Mode = CAN_MODE_NORMAL;
+  hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
+  hcan.Init.TimeSeg1 = CAN_BS1_5TQ;
+  hcan.Init.TimeSeg2 = CAN_BS2_2TQ;
+  hcan.Init.TimeTriggeredMode = DISABLE;
+  hcan.Init.AutoBusOff = DISABLE;
+  hcan.Init.AutoWakeUp = DISABLE;
+  hcan.Init.AutoRetransmission = DISABLE;
+  hcan.Init.ReceiveFifoLocked = DISABLE;
+  hcan.Init.TransmitFifoPriority = DISABLE;
+  if (HAL_CAN_Init(&hcan) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN CAN_Init 2 */
     /*##-2- Configure the CAN Filter ###########################################*/
     sFilterConfig.FilterBank = 0;
     sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
@@ -207,21 +304,24 @@ static void MX_CAN_Init(void) {
     sFilterConfig.FilterMaskIdLow = 0x0000;
     sFilterConfig.FilterFIFOAssignment = CAN_RX_FIFO0;
     sFilterConfig.FilterActivation = ENABLE;
-    sFilterConfig.SlaveStartFilterBank = 14;
+    sFilterConfig.SlaveStartFilterBank = 14; // how many filters to assign to the CAN1 (master can)
 
-    if (HAL_CAN_ConfigFilter(&CanHandle, &sFilterConfig) != HAL_OK) {
+    if (HAL_CAN_ConfigFilter(&hcan, &sFilterConfig) != HAL_OK) {
         /* Filter configuration Error */
         Error_Handler();
     }
 
     /*##-3- Start the CAN peripheral ###########################################*/
-    if (HAL_CAN_Start(&CanHandle) != HAL_OK) {
+    if (HAL_CAN_Start(&hcan) != HAL_OK) {
         /* Start Error */
         Error_Handler();
     }
 
-    /*##-4- Activate CAN RX notification #######################################*/
-    if (HAL_CAN_ActivateNotification(&CanHandle, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK) {
+    const uint32_t error_filter = CAN_IT_ERROR_WARNING | CAN_IT_ERROR_PASSIVE | CAN_IT_BUSOFF |
+			CAN_IT_LAST_ERROR_CODE | CAN_IT_ERROR;
+
+    if (HAL_CAN_ActivateNotification(&hcan, error_filter) != HAL_OK) {
+
         /* Notification Error */
         Error_Handler();
     }
@@ -234,74 +334,77 @@ static void MX_CAN_Init(void) {
     TxHeader.DLC = 2;
     TxHeader.TransmitGlobalTime = DISABLE;
 
-    /* USER CODE END CAN_Init 2 */
+  /* USER CODE END CAN_Init 2 */
 
 }
 
 /**
- * @brief USART2 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_USART2_UART_Init(void) {
+  * @param None
+  * @brief USART2 Initialization Function
+  * @retval None
 
-    /* USER CODE BEGIN USART2_Init 0 */
+  */
+static void MX_USART2_UART_Init(void)
+{
 
-    /* USER CODE END USART2_Init 0 */
+  /* USER CODE BEGIN USART2_Init 0 */
 
-    /* USER CODE BEGIN USART2_Init 1 */
+  /* USER CODE END USART2_Init 0 */
 
-    /* USER CODE END USART2_Init 1 */
-    huart2.Instance = USART2;
-    huart2.Init.BaudRate = 115200;
-    huart2.Init.WordLength = UART_WORDLENGTH_8B;
-    huart2.Init.StopBits = UART_STOPBITS_1;
-    huart2.Init.Parity = UART_PARITY_NONE;
-    huart2.Init.Mode = UART_MODE_TX_RX;
-    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-    if (HAL_UART_Init(&huart2) != HAL_OK) {
-        Error_Handler();
-    }
-    /* USER CODE BEGIN USART2_Init 2 */
+  /* USER CODE BEGIN USART2_Init 1 */
 
-    /* USER CODE END USART2_Init 2 */
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
 
 }
-
 /**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
-static void MX_GPIO_Init(void) {
-    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-    /* GPIO Ports Clock Enable */
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-    __HAL_RCC_GPIOD_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    __HAL_RCC_GPIOB_CLK_ENABLE();
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
-    /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
-    /*Configure GPIO pin : B1_Pin */
-    GPIO_InitStruct.Pin = B1_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-    /*Configure GPIO pin : LD2_Pin */
-    GPIO_InitStruct.Pin = LD2_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
-    /* EXTI interrupt init*/
-    HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 }
 
@@ -310,16 +413,17 @@ static void MX_GPIO_Init(void) {
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
-void Error_Handler(void) {
-    /* USER CODE BEGIN Error_Handler_Debug */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1) {
     }
-    /* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
@@ -337,4 +441,5 @@ void assert_failed(uint8_t *file, uint32_t line)
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
+
 #endif /* USE_FULL_ASSERT */
